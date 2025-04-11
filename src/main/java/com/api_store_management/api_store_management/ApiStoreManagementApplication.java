50a3925ae0212fa977e572039a43a9362798c80b
Lucas Oliveira <camposdlucasoli@gmail.com>
2025-04-11 12:30:38 -0300

    File main

---
+++ src/main/java/com/api_store_management/api_store_management/ApiStoreManagementApplication.java
@@ -0,0 +1,13 @@
+package com.api_store_management.api_store_management;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiStoreManagementApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ApiStoreManagementApplication.class, args);
+    }
+
+}
