14559e8214bf74c988a7ac7f75ab7ee2c05314d3
Lucas Oliveira <camposdlucasoli@gmail.com>
2025-05-11 17:27:00 -0300

    feat: endpoint do usuario

--- src/main/java/com/api_store_management/api_store_management/controller/UserController.java
+++ src/main/java/com/api_store_management/api_store_management/controller/UserController.java
@@ -1,32 +1,66 @@
 package com.api_store_management.api_store_management.controller;
 
-import com.api_store_management.api_store_management.modal.dto.User.UserDto;
-import com.api_store_management.api_store_management.modal.entity.Usuario;
-import com.api_store_management.api_store_management.service.User.AddUser;
+import com.api_store_management.api_store_management.dto.UserDto;
+import com.api_store_management.api_store_management.modal.entity.User;
+import com.api_store_management.api_store_management.modal.repository.UserRepository;
+import com.api_store_management.api_store_management.service.UserService;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/api/user")
 public class UserController {
 
-    private final AddUser addUser;
+    private final UserService userService;
+    private final UserRepository userRepository;
 
-    public UserController(AddUser addUser) {
-        this.addUser = addUser;
+    public UserController(UserService userService, UserRepository userRepository) {
+        this.userService = userService;
+        this.userRepository = userRepository;
     }
 
     @PostMapping("/")
-    public ResponseEntity<String> createUser(@RequestBody UserDto userDto) {
-        Usuario request = addUser.addUser(userDto);
+    public ResponseEntity<User> createUser(@RequestBody UserDto userDto){
+        User userRequest = userService.addUser(userDto);
 
-        if (request != null){
-            return ResponseEntity.ok().body(request.toString());
+        if (userRequest != null){
+            return ResponseEntity.ok(userRequest);
         }
 
-        return ResponseEntity.badRequest().build();
+        return ResponseEntity.notFound().build();
+    }
+
+    @GetMapping("/")
+    public ResponseEntity<List<User>> getAllUsers(){
+        List<User> listUsers = userService.getAllUser();
+
+        if (listUsers != null && !listUsers.isEmpty()){
+            return ResponseEntity.ok(listUsers);
+        }
+
+        return ResponseEntity.notFound().build();
+    }
+
+    @PutMapping("/")
+    public ResponseEntity<User> updateUser(@PathVariable Integer id, @RequestBody UserDto userDto){
+        User updatedUser = userService.updateUser(id, userDto);
+
+        if (updatedUser != null){
+            return ResponseEntity.ok(updatedUser);
+        }
+
+        return ResponseEntity.notFound().build();
+    }
+
+    @DeleteMapping("/")
+    public ResponseEntity<Void> deleteAllUsers(){
+        if (userRepository.findAll().isEmpty()){
+            return ResponseEntity.notFound().build();
+        }
+
+        userService.deleteAllUsers();
+        return ResponseEntity.ok().build();
     }
 }
