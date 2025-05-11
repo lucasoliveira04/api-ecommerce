bea6ec87861c1518bac51805b45f9f115f1f7702
Lucas Oliveira <camposdlucasoli@gmail.com>
2025-05-11 17:27:49 -0300

    feat: usuario repository

--- src/main/java/com/api_store_management/api_store_management/modal/repository/UserRepository.java
+++ src/main/java/com/api_store_management/api_store_management/modal/repository/UserRepository.java
@@ -1,9 +1,9 @@
 package com.api_store_management.api_store_management.modal.repository;
 
-import com.api_store_management.api_store_management.modal.entity.Usuario;
+import com.api_store_management.api_store_management.modal.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface UserRepository extends JpaRepository<Usuario, Integer> {
+public interface UserRepository extends JpaRepository<User, Integer> {
 }
