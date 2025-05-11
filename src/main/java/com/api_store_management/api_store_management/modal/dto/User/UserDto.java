7cfeaf08281a5235ef55902b66c7782d6f04e620
Lucas Oliveira <camposdlucasoli@gmail.com>
2025-05-11 17:28:08 -0300

    arquivos xml

--- src/main/java/com/api_store_management/api_store_management/modal/dto/User/UserDto.java
+++
@@ -1,6 +0,0 @@
-package com.api_store_management.api_store_management.modal.dto.User;
-
-import java.io.Serializable;
-
-public record UserDto(String username, String email, String password) implements Serializable {
-}
--- src/main/java/com/api_store_management/api_store_management/modal/repository/UserProductRepository.java
+++
@@ -1,9 +0,0 @@
-package com.api_store_management.api_store_management.modal.repository;
-
-import com.api_store_management.api_store_management.modal.entity.UserProduct;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface UserProductRepository extends JpaRepository<UserProduct, Integer> {
-}
