8e7ac3ba68cfc041a443c5f13c05c194ba011199
Lucas Oliveira <camposdlucasoli@gmail.com>
2025-06-14 09:09:21 -0300

    feat: remove Product entity and related DTO and repository

--- src/main/java/com/api_store_management/api_store_management/modal/repository/ProductRepository.java
+++
@@ -1,9 +0,0 @@
-package com.api_store_management.api_store_management.modal.repository;
-
-import com.api_store_management.api_store_management.modal.entity.Product;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface ProductRepository extends JpaRepository<Product, Integer> {
-}
