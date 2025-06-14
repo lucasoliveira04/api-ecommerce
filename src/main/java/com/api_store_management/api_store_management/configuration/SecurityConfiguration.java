8341a5dc543d165d3b4c3aa97c0f8d6d33a4b7c7
Lucas Oliveira <camposdlucasoli@gmail.com>
2025-06-14 09:39:43 -0300

    configuration spring security

---
+++ src/main/java/com/api_store_management/api_store_management/configuration/SecurityConfiguration.java
@@ -0,0 +1,32 @@
+package com.api_store_management.api_store_management.configuration;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfiguration {
+
+    private static final String[] PUBLIC_URLS = {
+           "/api/user"
+    };
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http
+                .csrf(AbstractHttpConfigurer::disable)
+                .cors(Customizer.withDefaults())
+                .authorizeHttpRequests(auth -> auth
+                        .requestMatchers(PUBLIC_URLS).permitAll()
+                        .anyRequest().authenticated()
+                )
+                .httpBasic(Customizer.withDefaults());
+
+        return http.build();
+    }
+}
